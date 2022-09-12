86cafc95ba5bea98b5f9633fa0332a93f51963c7
NicoC0 <nicoconfaa@gmail.com>
2022-09-12 13:25:00 -0300

    redux for people and favorites

---
+++ src/redux/states/favorites.ts
@@ -0,0 +1,16 @@
+import { localStorageType, Person } from "@/models";
+import { getLocalStorage, setLocalStorage } from "@/utils";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState:Person[] = []
+
+export const favoritesSlice = createSlice({
+  name: 'favorites',
+  initialState: getLocalStorage(localStorageType.FAVORITES) ? JSON.parse(getLocalStorage(localStorageType.PEOPLE) as string) : initialState,
+  reducers: {
+    addFavorite: (state, action) => {
+      setLocalStorage(localStorageType.FAVORITES, state)
+      return action.payload
+    }
+  }
+})
---
+++ src/redux/states/people.ts
@@ -0,0 +1,16 @@
+import { localStorageType, Person } from "@/models";
+import { getLocalStorage, setLocalStorage } from "@/utils";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState:Person[] = []
+
+export const peopleSlice = createSlice({
+  name: 'people',
+  initialState: getLocalStorage(localStorageType.PEOPLE) ? JSON.parse(getLocalStorage(localStorageType.PEOPLE) as string) : initialState,
+  reducers: {
+    addPeople: (state, action) => {
+      setLocalStorage(localStorageType.PEOPLE, state)
+      return action.payload
+    }
+  }
+})
