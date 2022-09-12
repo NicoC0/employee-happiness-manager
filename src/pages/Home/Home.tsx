9a887b095f94abd8d6797c0d93852f9d249e8e87
NicoC0 <nicoconfaa@gmail.com>
2022-09-12 20:29:13 -0300

    render peopleTable component and move logic to that component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,82 +1,21 @@
-import React, { useState } from 'react';
-import { DataGrid, GridRenderCellParams } from '@mui/x-data-grid';
 import { People } from '@/data';
-import { Person } from '@/models';
-import { Checkbox } from '@mui/material';
-
+import { addPeople } from '@/redux/states';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { PeopleTable } from './components/PeopleTable';
 
 export interface HomeInterface {}
 
 const Home: React.FC<HomeInterface> = () => {
-	const [selectedEmployee, setSelectedEmployee] = useState<Person[]>([])
+	const dispatch = useDispatch()
 
-	// if true, checkbox is checked
-	const findPerson = (person: Person) => !!selectedEmployee.find(employee => employee.id === person.id)
+	useEffect(() => {
+		dispatch(addPeople(People))
+	}, [])
+	
 
-	const filterPerson = (person: Person) => selectedEmployee.filter(employee => employee.id !== person.id)
-
-	const handleOnChange = (person: Person) => {
-		setSelectedEmployee(findPerson(person) ? filterPerson(person) : [...selectedEmployee, person])
-	}
-
-	const pageSize = 5;
-	const columns = [
-		{
-			field: 'action',
-			type: 'actions',
-			sortable: false,
-			headerName: '',
-			width: 15,
-			renderCell: (params: GridRenderCellParams) => <>
-				{
-					// !!: transforms the resolution of that method into true or false
-					<Checkbox  size='small'
-						checked={findPerson(params.row)}
-						onChange={() => handleOnChange(params.row)}
-					/>
-				}
-			</>
-		},
-		{
-			field: 'name',
-			headerName: 'Name',
-			flex: 1,
-			minWidth: 150,
-			renderCell: (params: GridRenderCellParams) => <>
-				{params.value}
-			</>
-		},
-		{
-			field: 'category',
-			headerName: 'Category',
-			flex: 1,
-			minWidth: 150,
-			renderCell: (params: GridRenderCellParams) => <>
-				{params.value}
-			</>
-		},
-		{
-			field: 'company',
-			headerName: 'Company',
-			flex: 1,
-			minWidth: 150,
-			renderCell: (params: GridRenderCellParams) => <>
-				{params.value}
-			</>
-		}
-	]
 	return (
-		<DataGrid
-			rows={People}
-			columns={columns}
-			pageSize={pageSize}
-			rowsPerPageOptions={[pageSize]}
-			disableColumnSelector
-			disableSelectionOnClick
-			autoHeight
-			// similar to map, assigns an id to the row
-			getRowId={(row: any) => row.id}
-		/>
+		<PeopleTable />
 	);
 };
 
