89d607693e9e0a47df1c6b7eb00b8b972e0ce8a6
NicoC0 <nicoconfaa@gmail.com>
2022-09-12 23:21:34 -0300

    favorites modal with button and onClick function

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,38 @@
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Button, Toolbar, Typography } from '@mui/material';
 import React from 'react';
+import { CustomDialog } from '../CustomDialog';
+import { FavoriteTable } from './FavoriteTable';
+import StarIcon from '@mui/icons-material/Star';
+import { dialogOpenSubject$ } from '../CustomDialog/CustomDialog';
 
 
 export interface NavbarInterface {}
 
 const Navbar: React.FC<NavbarInterface> = () => {
+
+	const handleOnClick = () => {
+		dialogOpenSubject$.setSubject = true;
+	}
+
 	return (
-		<AppBar position="fixed">
-			<Toolbar>
-				<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-					Employee Happiness Manager
-				</Typography>
-			</Toolbar>
-		</AppBar>
+		<>
+			<CustomDialog>
+				<FavoriteTable />
+			</CustomDialog>
+			<AppBar position="fixed">
+				<Toolbar>
+					<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+						Employee Happiness Manager
+					</Typography>
+					<Button
+						variant="contained"
+						onClick={handleOnClick}
+					>
+						<StarIcon /> Favorites
+					</Button>
+				</Toolbar>
+			</AppBar>
+		</>
 	);
 };
 
